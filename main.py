5b176809bf775847987c32864b78a83e14d6f9e1
isak lundgren <isak.leopold@gmail.com>
2023-11-13 12:59:36 +0100

    Added forces, added mass and solving

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 d_p = 0.5 * 1e-3  # m
 rho_p = 2560  # kg/m^3
 m_p = rho_p * 4 / 3 * np.pi * (d_p / 2) ** 3
+Re_p_max = 0.1  # The maximum reynolds particle number where stokes flow occurs
 
 # Fluid properties
 mu_f = 1.0016 * 1e-3  # Pa s
@@ -22,13 +23,29 @@ V_0 = 0  # m/s
 g = 9.82  # m/s^2
 
 # Initialize quantities
-t = t_0
-V = [V_0]
-Re_p = [rho_f * np.abs(U_f - V_0) * d_p / mu_f]
+N_steps = int((t_max - t_0) / dt)
+t = t_0 * np.ones(N_steps)
+V = V_0 * np.ones(N_steps)
+Re_p = rho_f * np.abs(U_f - V_0) * d_p / mu_f * np.ones(N_steps)
 
 # Run solver
-for i in range(0, int((t_max - t_0) / dt)):
+for i in range(1, N_steps):
     # Explicit solver, calculating from previous timestep
 
     # Caluclate forces on the particle
-    F_D = 3 * np.pi * mu_f * d_p * (U_f - V[i])
+    F_D = 3 * np.pi * mu_f * d_p * (U_f - V[i-1])  # N Drag force
+    F_g = -m_p * g  # N Gravitational force
+    # No lift force since there's no horizontal movement
+    F_tot = F_D + F_g
+
+    # Since there's no fluid movement, calculate added mass as a simple addition to particle mass
+    m_added = 1 / 2 * m_p * rho_f / rho_p
+    m_tot = m_p + m_added
+
+    # Do timestep
+    t[i] = t[i-1] + dt
+    V[i] = V[i-1] + dt * F_tot / m_tot
+
+    # Calculate reynolds number to ensure stokes flow
+    Re_p[i] = rho_f * np.abs(U_f - V[i]) * d_p / mu_f
+
